306c05cf9b3c94d48b403c4783d6fa576ed9fbdd
jheeju <jheeju.dev@gmail.com>
2021-08-22 20:35:18 +0900

    Check Length, Email & Password Match

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username'); 
 const email = document.getElementById('email'); 
 const password = document.getElementById('password'); 
-const password2 = document.getElementById('fopassword2'); 
+const password2 = document.getElementById('password2'); 
 
 // Show input error message
 function showError(input, message) {
@@ -19,9 +19,13 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(input.value.trim())) {
+    showSuccess(input)
+  } else {
+    showError(input, 'Email is not valid')
+  }
 }
 
 // Get field name
@@ -30,8 +34,9 @@ function getFieldName (input) {
 }
 
 // Check required fields
-function checkRequired(inputArr) {
+function checkRequired (inputArr) {
   inputArr.forEach(function(input) {
+    console.log("input", input)
     if (input.value.trim() === "") {
       showError(input, `${getFieldName(input)} is required`)
     } else {
@@ -40,9 +45,30 @@ function checkRequired(inputArr) {
   })
 }
 
+function checkLength (input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${min} characters`)
+  } else {
+    showSuccess(input)
+  }
+}
+
+// Check password match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
+}
+
 // Event listners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   
   checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25)
+  checkEmail(email)
+  checkPasswordMatch(password, password2)
 })
